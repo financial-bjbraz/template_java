caac89bbdacfe2d30f958114884e6ab12d540222
bjbrazeirelli <bjbrazeirelli@gmail.com>
2023-10-17 22:11:49 -0300

    initial

---
+++ src/main/kotlin/br/com/bjbraz/repository/AuthRepository.kt
@@ -0,0 +1,14 @@
+package br.com.bjbraz.repository
+
+import br.com.bjbraz.entity.auth.AuthEntity
+import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository
+import org.springframework.stereotype.Repository
+import java.util.*
+
+@Repository
+interface AuthRepository : ReactiveMongoRepository<AuthEntity, String> {
+
+      fun findByDtRef(dtRef: String?): Optional<AuthEntity?>?
+
+}
---
+++ src/test/java/br/com/bjbraz/audit/exchange/SimpleTests.java
@@ -0,0 +1,4 @@
+package br.com.bjbraz.audit.exchange;
+
+public class SimpleTests {
+}
